870e6c55d3395d533c6d8727eb4374b156717940
Muffeena <victoriaakintunde88@gmail.com>
2025-09-13 18:31:22 +0100

    Add files via upload

---
+++ BasicCalculatorProgram.py
@@ -0,0 +1,12 @@
+A = int(input('Enter your first number')) 
+B = int(input('Enter your second number'))
+C = input('Enter your Operation')
+
+if '+' in C:
+    print(A+B)
+elif '-' in C:
+    print(A-B)   
+elif '*' in C:
+    print(A*B) 
+elif '/' in C:
+    print(A/B)      
